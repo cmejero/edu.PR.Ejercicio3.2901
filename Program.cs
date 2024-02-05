b6e1c0188625e87b4cb5c91fd0e22e61e71fb903
csi23-cmejero <csi23-cmejero@colegioaltair.net>
2024-02-05 14:07:36 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace edu.PR.Ejercicio3._2901
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaActual = DateTime.Now;
+            Console.WriteLine(fechaActual.ToString());
+            DateTime fechaModificada;
+           
+
+            Random rd = new Random();
+
+            int aniosAleatorio = rd.Next(0,5);
+            fechaModificada = fechaActual.AddDays(1); 
+            fechaModificada = fechaActual.AddMonths(-24);
+            fechaModificada = fechaActual.AddYears(aniosAleatorio);
+
+            Console.WriteLine(fechaModificada.ToString());
+
+            if(fechaActual>fechaModificada)
+            {
+                Console.WriteLine("El hoy es más grande");
+            }
+            else
+            {
+                Console.WriteLine("El hoy es más pequeño");
+            }
+
+
+        }
+        
+}
+}
